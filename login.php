a66fa1dbc842c422f86a200118d9a8c5c5358256
soyo <164558822+oyossss@users.noreply.github.com>
2025-03-02 02:34:32 +0900

    로그인 디자인 추가

--- login.php
+++ login.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel = "stylesheet" href = "login.css">
+
 
     <style>
         /* 화면 중앙 정렬 */
